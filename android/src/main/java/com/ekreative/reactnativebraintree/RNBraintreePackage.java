bc764cdcb7bd534891f7f6e4ef84a18f77b92eb7
Bohdan Rybak <bohdan.rybak@ekreative.com>
2021-09-08 17:52:49 +0300

    fix: revert RNBraintreePackage file

--- android/src/main/java/com/ekreative/reactnativebraintree/RNBraintreePackage.java
+++ android/src/main/java/com/ekreative/reactnativebraintree/RNBraintreePackage.java
@@ -2,14 +2,15 @@
 
 package com.ekreative.reactnativebraintree;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNBraintreePackage implements ReactPackage {
     @Override
