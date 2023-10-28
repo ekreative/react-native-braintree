9d09fef2b27cde21d4a47853fbb7aa5cf510ba0c
Ivan Stelmakh <stelmakh.developer@gmail.com>
2023-10-28 15:20:31 +0300

    build: add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    es2023: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
